b0c448b121a777c73335463b1a3a99ed9a9189a7
Benjamin Rancourt <benjamin.rancourt@usherbrooke.ca>
2020-10-01 15:01:40 -0400

    Remove forgetten PHP options from PHP

--- index.js
+++ index.js
@@ -34,17 +34,4 @@ module.exports = {
 
   // Indent lines with tabs instead of spaces
   useTabs: false, // Default: false
-
-  /**
-   * Options from the the PHP plugin
-   * See https://github.com/prettier/plugin-php
-   */
-  // Prettier will move open brace for code blocks (classes, functions and methods) onto same line
-  braceStyle: '1tbs', // Default: "psr-2"
-
-  // PHP version we are currently using
-  phpVersion: '7.3', // Default: "5.4"
-
-  // Trailing commas will be added wherever possible
-  trailingCommaPHP: true, // Default: true
 };
